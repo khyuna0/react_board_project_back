6ebbbed07b3854b9aab7c2ebf55b2e9e6b912e0d
khyuna0 <khohyuna0102@gmail.com>
2025-10-13 11:13:29 +0900

    리액트 백엔드 설정

---
+++ src/main/java/com/khyuna0/home/Khyuna0ReactBoardApplication.java
@@ -0,0 +1,13 @@
+package com.khyuna0.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Khyuna0ReactBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Khyuna0ReactBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khyuna0/home/Khyuna0ReactBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khyuna0.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Khyuna0ReactBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
