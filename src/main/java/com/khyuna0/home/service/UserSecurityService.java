53cb828998ac77fa423ab553b7630745eb9cb666
khyuna0 <khohyuna0102@gmail.com>
2025-10-13 17:42:31 +0900

    시큐리티 서비스 수정

--- src/main/java/com/khyuna0/home/service/UserSecurityService.java
+++ src/main/java/com/khyuna0/home/service/UserSecurityService.java
@@ -16,6 +16,8 @@ public class UserSecurityService implements UserDetailsService {
 	private UserRepository userRepository;
 	
 	// Spring security -> 유저에게 받은 username과 password를 조회
+	// username이 존재하지 않으면 "사용자 없음"으로 에러 발생
+	// username이 존재하면 password 확인 -> 성공 시 권한 부여 (단, 비밀번호가 암호화 되어 있어야 함)
 	
 	@Override // DB에서 조회
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -24,7 +26,7 @@ public class UserSecurityService implements UserDetailsService {
 		return org.springframework.security.core.userdetails.User
 				.withUsername(siteUser.getUsername())
 				.password(siteUser.getPassword())
-				.authorities("USER")
-				.build();
+				.authorities("USER") // 권한 부여
+				.build(); // UserDetails 객체 생성 후 반환
 	}
 }
