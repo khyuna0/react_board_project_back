145c55859398329ebe02dc2153f2534644e37ab8
khyuna0 <khohyuna0102@gmail.com>
2025-10-14 12:54:49 +0900

    글 조회 매핑

--- src/main/java/com/khyuna0/home/controller/BoardController.java
+++ src/main/java/com/khyuna0/home/controller/BoardController.java
@@ -1,12 +1,14 @@
 package com.khyuna0.home.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,8 @@ import com.khyuna0.home.entity.Board;
 import com.khyuna0.home.entity.SiteUser;
 import com.khyuna0.home.repository.BoardRepository;
 import com.khyuna0.home.repository.UserRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/api/board")
@@ -48,5 +52,20 @@ public class BoardController {
 		boardRepository.save(board);
 
 		return ResponseEntity.ok(board);
+	}
+	
+	// 특정 게시글 아이디로 글 조회
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getPost(@PathVariable("id") Long id) {
+//		Board board = boardRepository.findById(id)
+//				.orElseThrow(()-> new EntityNotFoundException("해당 글 없음"));
+		
+		Optional<Board> board = boardRepository.findById(id);
+		if(board.isPresent()) { // 글 조회 성공
+			return ResponseEntity.ok(board.get()); // 해당 아이디 글 반환
+		} else { // 해당 글 조회 실패
+			return ResponseEntity.status(404).body("해당 게시글은 존재하지 않습니다.");
+		}
+		
 	}
 }
